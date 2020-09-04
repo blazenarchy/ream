a7c8156dc409284d968c6556b677e4845c1cb524
wnuke <wnuke@wnuke.dev>
2020-09-03 21:44:49 -0700

    improved real name command

--- src/main/java/dev/wnuke/nukestack/commands/RealNameCommand.java
+++ src/main/java/dev/wnuke/nukestack/commands/RealNameCommand.java
@@ -2,10 +2,14 @@ package dev.wnuke.nukestack.commands;
 
 import dev.wnuke.nukestack.NukeStack;
 import dev.wnuke.nukestack.PlayerDataUtilities;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RealNameCommand implements CommandExecutor {
     NukeStack plugin;
@@ -14,24 +18,38 @@ public class RealNameCommand implements CommandExecutor {
         this.plugin = plugin;
     }
 
-    private String getRealName(String nick) {
-        String noFormatNick = NickCommand.unFormatNick(nick);
+    private HashMap<String, String> getRealNames() {
+        HashMap<String, String> nickNames = new HashMap<>();
+        String playerNick;
         for (Player player : plugin.getServer().getOnlinePlayers()) {
-            if (NickCommand.unFormatNick(PlayerDataUtilities.loadPlayerData(player.getUniqueId()).getNickName()).equals(noFormatNick)) {
-                return player.getName();
+            playerNick = PlayerDataUtilities.loadPlayerData(player.getUniqueId()).getNickName();
+            if (!playerNick.isEmpty()) {
+                nickNames.put(playerNick, player.getName());
             }
         }
-        return "";
+        return nickNames;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        HashMap<String, String> nickNames = getRealNames();
         if (args.length > 0) {
-            String getResult = getRealName(args[0]);
-            if (getResult.equals("")) {
+            String realName = null;
+            for (Map.Entry<String, String> nick : nickNames.entrySet()) {
+                if (NickCommand.unFormatNick(nick.getKey()).equals(NickCommand.unFormatNick(args[0]))) {
+                    realName = nick.getValue();
+                    break;
+                }
+            }
+            if (realName == null) {
                 sender.sendMessage("No online players with name \"" + args[0] + "\".");
             } else {
-                sender.sendMessage(args[0] + " is " + getResult + ".");
+                sender.sendMessage(args[0] + " is " + realName + ".");
+            }
+        } else {
+            sender.sendMessage(ChatColor.YELLOW + "List of nicknames and who they are:");
+            for (Map.Entry<String, String> nick : nickNames.entrySet()) {
+                sender.sendMessage("  " + nick.getKey() + ChatColor.YELLOW + " is " + ChatColor.RESET + nick.getValue());
             }
         }
         return true;
