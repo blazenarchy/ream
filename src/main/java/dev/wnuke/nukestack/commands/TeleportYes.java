624cc065ff36a37ead28ac3f58ae79df1ed6b0fe
wnuke <wnuke@wnuke.dev>
2020-09-10 09:08:03 -0700

    do a double teleport to avoid players being found by exploits in tpa

--- src/main/java/dev/wnuke/nukestack/commands/TeleportYes.java
+++ src/main/java/dev/wnuke/nukestack/commands/TeleportYes.java
@@ -2,15 +2,18 @@ package dev.wnuke.nukestack.commands;
 
 import dev.wnuke.nukestack.GeneralUtilities;
 import dev.wnuke.nukestack.NukeStack;
+import dev.wnuke.nukestack.player.LastLocation;
 import dev.wnuke.nukestack.player.PlayerData;
 import dev.wnuke.nukestack.player.PlayerDataUtilities;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class TeleportYes implements CommandExecutor {
@@ -19,7 +22,7 @@ public class TeleportYes implements CommandExecutor {
         if (!(sender instanceof ConsoleCommandSender)) {
             if (args.length > 0) {
                 for (Player player : NukeStack.PLUGIN.getServer().getOnlinePlayers()) {
-                    if (player.getPlayerListName().equals(args[0])) {
+                    if (player.getName().equals(args[0])) {
                         if (NukeStack.teleportRequests.get(player.getUniqueId()) == ((Player) sender).getUniqueId()) {
                             UUID playerID = player.getUniqueId();
                             NukeStack.teleportRequests.remove(playerID);
@@ -32,10 +35,24 @@ public class TeleportYes implements CommandExecutor {
                             }
                             player.sendMessage(ChatColor.GREEN + "Teleport request accepted, teleporting...");
                             sender.sendMessage(ChatColor.GREEN + "Teleporting...");
-                            GeneralUtilities.hidePlayer(player);
-                            NukeStack.playerPosTracking.remove(playerID);
                             Player sendingPlayer = ((Player) sender).getPlayer();
                             if (sendingPlayer == null) return false;
+                            GeneralUtilities.hidePlayer(player);
+                            NukeStack.playerPosTracking.remove(playerID);
+                            playerData.setLogoutLocation(LastLocation.fromLocation(sendingPlayer.getLocation()));
+                            double worldBorder = sendingPlayer.getWorld().getWorldBorder().getSize();
+                            Random random = new Random();
+                            while (true) {
+                                double[] location = {0, 0, 0};
+                                for (int i = 0; i < location.length; i++) {
+                                    location[i] = worldBorder * random.nextDouble() - worldBorder / 2;
+                                }
+                                Location farPlace = new Location(sendingPlayer.getWorld(), location[0], location[1], location[2]);
+                                if (farPlace.getNearbyPlayers(8000).isEmpty()) {
+                                    player.teleport(farPlace);
+                                    break;
+                                }
+                            }
                             player.teleport(sendingPlayer);
                             NukeStack.playerPosTracking.remove(playerID);
                             GeneralUtilities.unhidePlayer(player);
